09a9c30b6077268721229ce2f31d703a02e80bb1
Dan Waehner <61367502+dwaehner27@users.noreply.github.com>
2021-03-09 20:48:29 -0600

    Update pypoll.py

--- pypoll.py
+++ pypoll.py
@@ -2,7 +2,8 @@ import os
 import csv
 csvpath = os.path.join('Resources', 'election_data.csv')
 
-total_votes = 0
+votes = 0
+total_votes = []
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -10,17 +11,6 @@ with open(csvpath) as csvfile:
 
     for row in csvreader:
         #find total number of votes cast
-        total_votes += 1
-        print(total_votes)  
-
-
-
-#list of canidates who recieved votes
-
-#percentage of votes per canidate
-
-#total number of votes each canidate won
-
-#winner of the election
-
-#export text file with results
+        votes += 1
+        total_votes.append(votes) 
+print(f"total votes: {max(total_votes)}")
